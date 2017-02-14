09ee8913780eb3eae48757883421dea91d0d35f3
veroniquesenecal <vero.senecal1993@gmail.com>
2017-02-13 21:54:14 -0500

    Travail à présenter à Jean-Hughes Roy

---
+++ devoir1.py
@@ -0,0 +1,20 @@
+#coding : utf-8
+
+for annee in range(30000,100000):
+    print(annee)
+
+for annee in range(0,10):
+    print("0000{}".format(annee))
+    
+for annee in range(10,100):
+    print("000{}".format(annee))
+
+for annee in range(100,1000):
+    print("00{}".format(annee))
+    
+for annee in range(1000,10000):
+    print("0{}".format(annee))
+    
+for annee in range(10000,18000):
+    print(annee)
+ 
